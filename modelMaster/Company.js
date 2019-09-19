8b4fd9dd48cabd7c320ae2d6062f5959c7d02d9d
Luqman <luqman.zakariya@gmail.com>
2019-09-19 19:22:48 +0700

    initial commit server

---
+++ modelMaster/Company.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const {generatePassword} = require('../helpers/bcryptjs')
+
+let CompanySchema = new Schema ({
+  openTime: {
+    type: Date,
+    required: [true, "date cannot be empty"]
+  },
+  closeTime: {
+    type: Date,
+    required: [true, "date cannot be empty"]
+  },
+  location: {
+    type: String
+  },
+  email: {
+    type: String,
+    validate: [{
+      validator: function validateFormat(email){
+        let format = /\S+@\S+\.\S+/;
+        return format.test(email)
+      },
+      message : props => `${props.value} is not a valid email`
+    },{
+      validator: function checkUnique(email){
+        return Company.findOne({email: this.email})
+          .then((company)=>{
+            if (company){
+              return false
+            }
+            else {
+              return true
+            }
+          })
+          .catch((err)=>{
+            return false
+          })
+      },
+      message : props => `Email ${props.value} has been used`
+    }],
+    required: [true, 'email cannot be emtpy']
+  },
+  password: {
+    type: String,
+    minlength:[8,"Password minimum length is 8"],
+    required: [true, 'password cannot be empty']
+  },
+},{timestamps: true})
+
+CompanySchema.pre('save', function(next){
+  this.password = generatePassword(this.password)
+  next()
+})
+
+let Company = mongoose.model ('Company', CompanySchema)
+
+module.exports = Company
